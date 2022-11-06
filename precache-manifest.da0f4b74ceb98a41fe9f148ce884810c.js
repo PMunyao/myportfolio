789dd589d48836509eb89055ccdbcd312082723d
Code Sandbox <hello@codesandbox.io>
2022-11-06 19:39:18 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.da0f4b74ceb98a41fe9f148ce884810c.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "923820a1a4d23e1a2409",
+    "url": "https://pmunyao.github.io/csb-dtozy7/static/js/main.568bb899.chunk.js"
+  },
+  {
+    "revision": "b8b05286fd9dd1fcea9e",
+    "url": "https://pmunyao.github.io/csb-dtozy7/static/js/runtime~main.59e00663.js"
+  },
+  {
+    "revision": "b5aae1808c19f76220a5",
+    "url": "https://pmunyao.github.io/csb-dtozy7/static/js/2.30c5a143.chunk.js"
+  },
+  {
+    "revision": "358a2c4490b718423c208a868f1f7abe",
+    "url": "https://pmunyao.github.io/csb-dtozy7/index.html"
+  }
+];
